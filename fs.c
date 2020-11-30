6b6376b3ee865c99dd12a76a8bd168a44191e6fd
Mrinal Ghosh <ghoshm@bu.edu>
2020-11-30 15:33:48 -0500

    make_fs

--- fs.c
+++ fs.c
@@ -1,2 +1,89 @@
-#include "disk.h"
 #include "fs.h"
+
+#include "disk.h"
+#include "string.h"
+
+/* global variables */
+struct superblock_t fs;
+struct fd_t fildes[MAX_FILDES];
+short *FAT;
+struct dir_entry_t *DIR;
+bool mounted = false;
+
+int make_fs(char *disk_name) {
+    if (make_disk(disk_name) == -1 || open_disk(disk_name) == -1) {
+        return -1;
+    }
+
+    /* initialize meta-information */
+    superblock_t fs_;
+    fs_.fat_idx = 1;                                                   // start after super block
+    fs_.fat_len = (sizeof(short) * DISK_BLOCKS) / BLOCK_SIZE;          // 2*8192/4096 = 4
+    fs_.dir_idx = fs_.fat_idx + fs_.fat_len;                           // 1 + 4 = 5
+    fs_.dir_len = (sizeof(dir_entry_t) * MAX_FILES) / BLOCK_SIZE + 1;  // 32*64/4096 + 1 = 1
+    fs_.data_idx = DISK_BLOCKS / 2;                                    // TODO: SAVE ALL THIS SPACE BY BEGINNING DATA SEGMENT AFTER FAT AND DIR = fs_.dir_idx+fs_.dir_len
+
+    /* copy meta-information to disk blocks */
+    char *buffer = calloc(1, BLOCK_SIZE);  // temporary buffer
+
+    /* block write super block */
+    memcpy((void *)buffer, (void *)&fs_, sizeof(superblock_t));
+    block_write(0, buffer);
+
+    /* block write FAT */
+    int i;
+    for (i = 0; i < fs_.fat_len; ++i) {
+        memset((void *)buffer, FREE, BLOCK_SIZE);  // sets all to 0xf...f (CHARACTERWISE - check whether this works)
+
+        if (i == 0) {  // reserve meta-segment
+            int j;
+            for (j = 0; j < fs_.data_idx; ++j) {
+                memcpy((void *)(buffer + j * sizeof(short)), (void *)RESERVED, sizeof(short));
+            }
+        }
+
+        block_write(fs_.fat_idx + i, buffer);
+    }
+
+    /* block write DIR */
+    dir_entry_t empty = {.used = false, .name[0] = '\0', .size = 0, .head = -1, .ref_cnt = 0}; /* placeholder empty directory entry*/
+
+    for (i = 0; i < MAX_FILES; ++i) {
+        memcpy((void *)(buffer + i * sizeof(dir_entry_t)), (void *)&empty, sizeof(dir_entry_t)); /* populate directory block buffer with empty entries */
+    }
+
+    block_write(fs_.dir_idx, buffer);
+
+    free(buffer);
+    mounted = true;
+    close_disk(disk_name);  // TODO: need to close disk after writing?
+
+    return 0;
+}
+
+int mount_fs(char *disk_name) {
+    if (open_disk(disk_name) == -1) {
+        return -1;
+    }
+
+    // fs = calloc(1, sizeof(superblock_t)); // don't need to dynamically allocate - should super block be static then?
+    DIR = calloc(MAX_FILES, sizeof(dir_entry_t));
+    FAT = calloc(DISK_BLOCKS, sizeof(short));
+    char *buffer = calloc(1, BLOCK_SIZE);
+}
+
+int umount_fs(char *disk_name) {
+}
+
+// int fs_open(char *name);
+// int fs_close(int fildes);
+// int fs_create(char *name);
+// int fs_delete(char *name);
+
+// int fs_read(int fildes, void *buf, size_t nbyte);
+// int fs_write(int fildes, void *buf, size_t nbyte);
+
+// int fs_get_filesize(int fildes);
+// int fs_listfiles(char ***files);
+// int fs_lseek(int fildes, off_t offset);
+// int fs_truncate(int fildes, off_t length);
--- fs.h
+++ fs.h
@@ -1,7 +1,47 @@
 #ifndef FS_H
 #define FS_H
 
+#include <stdbool.h>
+#include <stdlib.h>
 #include <sys/types.h>
+
+#define error(message)      \
+    {                       \
+        perror(message);    \
+        exit(EXIT_FAILURE); \
+    }
+
+#define MAX_FILES 64            // maximum files at any given time
+#define MAX_FILDES 32           // maximum file descriptors per file
+#define MAX_FILENAME 15         // maximum 15 character filenames
+#define MAX_FILESIZE (1 << 24)  // maximum file size 16 MB = 16777216 B
+#define FREE -1                 // not allocated to a file
+#define EOF -2                  // end of file code
+#define RESERVED -3             // FAT, DIR, superblock not to be overwritten
+
+/* defined in disk.h - DISK_BLOCKS 8192 - BLOCK_SIZE 4096 */
+
+typedef struct superblock_t {  // hardcoded to block 0
+    int dir_idx;               // head of directory
+    int dir_len;               // number of blocks of directory
+    int fat_idx;               // head of FAT
+    int fat_len;               // number of blocks of FAT
+    int data_idx;              // first block of file data
+} superblock_t;
+
+typedef struct dir_entry_t {
+    bool used;                    // is this file-"slot" in use
+    char name[MAX_FILENAME + 1];  // mmmm.. donuts
+    int size;                     // file size (bytes)
+    int head;                     // first data block of file
+    int ref_cnt;                  // how many open fds (ref_cnt > 0) -> cannot delete file
+} dir_entry_t;
+
+typedef struct fd_t {  // file descriptor
+    bool used;         // fd in use
+    int file;          // first block of the file (f) to which fd refers to
+    int offset;        // position of fd within f - dynamically updated
+} fd_t;
 
 int make_fs(char *disk_name);
 int mount_fs(char *disk_name);
