882969ffc169a3b6d9f868b73c28b70405eb134b
Mrinal Ghosh <ghoshm@bu.edu>
2020-12-01 20:05:48 -0500

    fs_read

--- fs.c
+++ fs.c
@@ -5,71 +5,116 @@
 
 #include "disk.h"
 
+#define DEBUG 1
+
 /* global variables */
-struct superblock_t fs;
-struct fd_t fildes[MAX_FILDES];
+struct superblock fs;
+struct fd_t fd[MAX_FILDES];  // renamed to prevent collision with argument fildes
 int *FAT;
-struct dir_entry_t *DIR;
+struct dir_entry *DIR;
 bool mounted = false;  // TODO: is this even needed as a sanity check?
 
 /* EOF already defined in stdio.h as -1 */
-const int FREE = -2;      // not allocated to a file
-const int RESERVED = -3;  // FAT, DIR, superblock not to be overwritten // DO I NEED THIS OR CAN I JUSST USE EOF AGAIN
+const int FREE = -2;  // not allocated to a file
+// const int RESERVED = -3;  // FAT, DIR, superblock not to be overwritten // DO I NEED THIS OR CAN I JUSST USE EOF AGAIN
 
-static void DEBUG_DIR(int n) { printf("DIR entry %d\n---------8<--------\nused:%d\nfilename:%s\nsize:%d\nhead:%d\nref_count:%d\n---------8<--------\n", n, DIR[n].used, DIR[n].name, DIR[n].size, DIR[n].head, DIR[n].ref_count); }
+static void debug_dir(int n) {
+    if (DEBUG) {
+        printf("\nDIR entry %d\n---------8<--------\nused:%d\nfilename:%s\nsize:%d\nhead:%d\nref_count:%d\n---------8<--------\n",
+               n, DIR[n].used, DIR[n].name, DIR[n].size, DIR[n].head, DIR[n].ref_count);
+    }
+}
+
+static void debug_fat(int *fat) {
+    if (DEBUG) {
+        printf("FAT\n-------------------\n");
+        int i, j, n = 32;
+        for (i = 0; i < DISK_BLOCKS / n; ++i) {
+            for (j = 0; j < n; ++j)
+                printf("%d  ", fat[i * n + j]);
+            printf("\n");
+        }
+    }
+}
+
+static int directory_index(int fildes) {
+    int idx;
+    for (idx = 0; idx < MAX_FILES; ++idx) {
+        if (DIR[idx].used == true && DIR[idx].head == fd[fildes].file) {
+            return idx;
+        }
+    }
+    return -1;
+}
 
 int make_fs(char *disk_name) {
-    if (make_disk(disk_name) == -1 || open_disk(disk_name) == -1) {
-        perror("make_disk");
+    if (make_disk(disk_name) == -1) {
+        perror("make_fs: make_disk");
+        return -1;
+    }
+
+    if (open_disk(disk_name) == -1) {
+        perror("make_fs: open_disk");
         return -1;
     }
 
     /* initialize meta-information */
-    superblock_t fs_;
+    superblock fs_;
     fs_.dir_idx = 1;
-    fs_.dir_len = (sizeof(dir_entry_t) * MAX_FILES) / BLOCK_SIZE + 1;  // 32 * 64 / 4096 + 1 = 1
-    fs_.fat_idx = fs_.dir_idx + fs_.dir_len;                           // 1 + 1 = 2
-    fs_.fat_len = (sizeof(int) * DISK_BLOCKS) / BLOCK_SIZE;            // 4 * 8192 / 4096 = 8
-    fs_.data_idx = fs_.fat_idx + fs_.fat_len;                          // 2 + 8 = 10 | SAVE SPACE BY BEGINNING DATA SEGMENT AFTER FAT AND DIR = fs_.dir_idx+fs_.dir_len
+    fs_.dir_len = (sizeof(dir_entry) * MAX_FILES) / BLOCK_SIZE + 1;  // 32 * 64 / 4096 + 1 = 1
+    fs_.fat_idx = fs_.dir_idx + fs_.dir_len;                         // 1 + 1 = 2
+    fs_.fat_len = (sizeof(int) * DISK_BLOCKS) / BLOCK_SIZE;          // 4 * 8192 / 4096 = 8
+    fs_.data_idx = fs_.fat_idx + fs_.fat_len;                        // 2 + 8 = 10 | SAVE SPACE BY BEGINNING DATA SEGMENT AFTER FAT AND DIR = fs_.dir_idx+fs_.dir_len
 
     /* copy meta-information to disk blocks */
     char *buffer = calloc(1, BLOCK_SIZE);  // temporary buffer
 
     /* block write super block */
-    memcpy((void *)buffer, (void *)&fs_, sizeof(superblock_t));
-    if (block_write(0, buffer) == -1) return -1;
+    memcpy((void *)buffer, (void *)&fs_, sizeof(superblock));
+    if (block_write(0, buffer) == -1) {
+        perror("mount_fs: block_read()");
+        return -1;
+    }
 
     /* block write DIR */
-    dir_entry_t empty = {.used = false, .name[0] = '\0', .size = 0, .head = -1, .ref_count = 0}; /* placeholder empty entry */
+    dir_entry empty = {.used = false, .name[0] = '\0', .size = 0, .head = -1, .ref_count = 0}; /* placeholder empty entry */
 
     int i;
     for (i = 0; i < MAX_FILES; ++i) {
-        memcpy((void *)(buffer + i * sizeof(dir_entry_t)), (void *)&empty, sizeof(dir_entry_t)); /* populate single directory block buffer with empty entries */
+        memcpy((void *)(buffer + i * sizeof(dir_entry)), (void *)&empty, sizeof(dir_entry)); /* populate single directory block buffer with empty entries */
     }
 
-    if (block_write(fs_.dir_idx, buffer) == -1) return -1;
+    if (block_write(fs_.dir_idx, buffer) == -1) {
+        perror("mount_fs: block_read()");
+        return -1;
+    }
 
     /* block write FAT */
     for (i = 0; i < fs_.fat_len; ++i) {
         // printf("-----------------BUFFER START------------------\n");
         int j;
         for (j = 0; j < BLOCK_SIZE / sizeof(int); ++j) {  // iterate over # of FAT entries in one block = 4096 / 4 = 1024
-            if (j + i * BLOCK_SIZE / sizeof(int) < fs_.data_idx) {
-                // printf("Marking element %d as RESERVED in buffer", j + i * BLOCK_SIZE / 4);
-                memcpy((void *)(buffer + j * sizeof(int)), (void *)&RESERVED, sizeof(int));
-                // printf("value = %d\n", *(buffer + j * sizeof(int)));
-            } else {
-                // printf("Marking element %d as FREE in buffer", j + i * BLOCK_SIZE / 4);
-                memcpy((void *)(buffer + j * sizeof(int)), (void *)&FREE, sizeof(int));
-                // printf("value = %d\n", *(buffer + j * sizeof(int)));
-            }
+
+            // if (j + i * BLOCK_SIZE / sizeof(int) < fs_.data_idx) {
+            //     // printf("Marking element %d as RESERVED in buffer", j + i * BLOCK_SIZE / 4);
+            //     memcpy((void *)(buffer + j * sizeof(int)), (void *)&RESERVED, sizeof(int));
+            //     // printf("value = %d\n", *(buffer + j * sizeof(int)));
+            // } else {
+            // printf("Marking element %d as FREE in buffer", j + i * BLOCK_SIZE / 4);
+            memcpy((void *)(buffer + j * sizeof(int)), (void *)&FREE, sizeof(int));
+            // printf("value = %d\n", *(buffer + j * sizeof(int)));
         }
 
-        if (block_write(fs_.fat_idx + i, buffer) == -1) return -1;
+        if (block_write(fs_.fat_idx + i, buffer) == -1) {
+            perror("mount_fs: block_read()");
+            return -1;
+        }
     }
 
-    free(buffer);
-    if (close_disk(disk_name) == -1) return -1;  // TODO: need to close disk after writing?
+    if (close_disk(disk_name) == -1) {  // TODO: do I need to close disk?
+        perror("mount_fs: block_read()");
+        return -1;
+    }
 
     return 0;
 }
@@ -80,38 +125,41 @@ int mount_fs(char *disk_name) {
         return -1;
     }
 
-    // fs = calloc(1, sizeof(superblock_t)); // don't need to dynamically allocate - should super block be a static  variable then?
-    DIR = calloc(MAX_FILES, sizeof(dir_entry_t));
-    FAT = calloc(DISK_BLOCKS, sizeof(int));
+    DIR = calloc(MAX_FILES, sizeof(dir_entry));  // 64 x 32
+    FAT = calloc(DISK_BLOCKS, sizeof(int));      // 8192 x 4
 
     char *buffer = calloc(1, BLOCK_SIZE);
 
     /* mount super block */
-    if (block_read(0, buffer) == -1) return -1;
-    memcpy((void *)&fs, (void *)buffer, sizeof(superblock_t));
+    if (block_read(0, buffer) == -1) {
+        perror("mount_fs: block_read()");
+        return -1;
+    }
+    memcpy((void *)&fs, (void *)buffer, sizeof(superblock));
 
     /* mount DIR */
-    if (block_read(fs.dir_idx, buffer) == -1) return -1;
-    memcpy((void *)(DIR), (void *)(buffer), BLOCK_SIZE);  // only 1 DIR block - don't need to iterate
+    if (block_read(fs.dir_idx, buffer) == -1) {
+        perror("mount_fs: block_read()");
+        return -1;
+    }
+    memcpy((void *)(DIR), (void *)(buffer), BLOCK_SIZE);  // only 1 DIR block
 
     /* mount FAT */
     int i;
-    for (i = 0; i < fs.fat_len; ++i) {
-        if (block_read(fs.fat_idx + i, buffer) == -1) return -1;
-        memcpy((void *)(FAT + i * BLOCK_SIZE), (void *)buffer, BLOCK_SIZE);  // can just copy entire block since it is a scalar multiple
+    for (i = 0; i < fs.fat_len; ++i) {  // iter over FAT blocks
+        if (block_read(fs.fat_idx + i, buffer) == -1) {
+            perror("mount_fs: block_read()");
+            return -1;
+        }
+        memcpy((void *)(FAT + i * BLOCK_SIZE / sizeof(int)), (void *)buffer, BLOCK_SIZE);  // can just copy entire block since it is a scalar multiple
     }
 
     /* initialize file descriptor array for local use */
     for (i = 0; i < MAX_FILDES; ++i) {  // iter over file descriptors
-        fildes[i] = (fd_t){.used = false, .file = -1, .offset = 0};
+        fd[i] = (fd_t){.used = false, .file = -1, .offset = 0};
     }
 
-    // TODO: THIS KEEPS GIVING MUNMAP_CHUNK(): INVALID POINTER WHAT DO
-    // free(buffer);
     mounted = true;
-
-    // TODO: SHOULD RETURN ZERO IF DOESN'T CONTAIN A VALID FILE SYSTEM - what to check for?
-
     return 0;
 }
 
@@ -119,7 +167,7 @@ int umount_fs(char *disk_name) {
     char *buffer = calloc(1, BLOCK_SIZE);
 
     /* write back superblock */
-    memcpy((void *)buffer, (void *)&fs, sizeof(superblock_t));
+    memcpy((void *)buffer, (void *)&fs, sizeof(superblock));
     if (block_write(0, buffer) == -1) return -1;
 
     /* write back DIR */
@@ -133,7 +181,7 @@ int umount_fs(char *disk_name) {
         if (block_write(fs.fat_idx + i, buffer) == -1) return -1;
     }
 
-    /* fildes not written since not persistent across mounts */
+    /* fildes not written to since not persistent across mounts */
 
     if (close_disk(disk_name) == -1) {
         perror("umount_fs: close_disk");
@@ -146,8 +194,8 @@ int umount_fs(char *disk_name) {
 int fs_open(char *name) {
     /* find first unused fd */
     int fd_idx, fd_cnt = 0;
-    for (fd_idx = 0; fd_idx < MAX_FILDES; ++fd_idx) {  // iter over fd in fildes array
-        if (fildes[fd_idx].used == false) {            // unused fd_idx found
+    for (fd_idx = 0; fd_idx < MAX_FILDES; ++fd_idx) {  // iter over descriptors in fildes array
+        if (fd[fd_idx].used == false) {                // unused fd_idx found
             break;
         }
         ++fd_cnt;
@@ -158,11 +206,11 @@ int fs_open(char *name) {
         return -1;
     }
 
-    /* find file in directory and populate fd */
+    /* find in directory and populate fd */
     int i;
     for (i = 0; i < MAX_FILES; ++i) {          // iter over files in directory
         if (strcmp(name, DIR[i].name) == 0) {  // file DIR entry found
-            fildes[fd_idx] = (fd_t){.used = true, .file = DIR[i].head, .offset = 0};
+            fd[fd_idx] = (fd_t){.used = true, .file = DIR[i].head, .offset = 0};
             ++DIR[i].ref_count;
             break;
         } else if (i == MAX_FILES - 1) {  // last iter and not broken out
@@ -171,10 +219,20 @@ int fs_open(char *name) {
         }
     }
 
-    return fd_idx;  // return file desciptor (index)
+    return fd_idx;  // return file desciptor (idx)
 }
 
-int fs_close(int fildes) { return 0; }
+int fs_close(int fildes) {
+    int i;
+    for (i = 0; i < MAX_FILES; ++i) {  // iter over directory entries
+        if (DIR[i].head == fd[fildes].file) {
+            fd[fildes] = (fd_t){.used = false, .file = -1, .offset = 0};  // unuse file descriptor
+            --DIR[i].ref_count;                                           // decrement references
+        }
+    }
+
+    return 0;
+}
 
 int fs_create(char *name) {
     if (strlen(name) > MAX_FILENAME) {
@@ -198,9 +256,11 @@ int fs_create(char *name) {
         return -1;
     }
 
+    debug_fat(FAT);
+
     /* find first free FAT block and mark as EOF - needed in DIR entry below */
     int head;
-    for (head = fs.data_idx; head < DISK_BLOCKS; ++head) {  // iter over data segment
+    for (head = fs.data_idx; head < DISK_BLOCKS; ++head) {  // iter over number of blocks
         if (FAT[head] == FREE) {
             FAT[head] = EOF;
             break;
@@ -213,8 +273,15 @@ int fs_create(char *name) {
     /* populate DIR */
     for (i = 0; i < MAX_FILES; ++i) {
         if (DIR[i].used == false) {  // first unused entry
-            DIR[i] = (dir_entry_t){.used = true, .name = name, .size = 0, .head = head, .ref_count = 0};
-            DEBUG_DIR(i);
+            // DIR[i] = (dir_entry){.used = true, .name = name, .size = 0, .head = head, .ref_count = 0}; // this does not work
+
+            DIR[i].used = true;
+            memcpy((void *)DIR[i].name, (void *)name, MAX_FILENAME);
+            DIR[i].size = 0;
+            DIR[i].head = head;
+            DIR[i].ref_count = 0;
+
+            debug_dir(i);
             break;
         }
     }
@@ -222,12 +289,109 @@ int fs_create(char *name) {
     return 0;
 }
 
-int fs_delete(char *name) { return 0; }
+int fs_delete(char *name) {
+    /* find DIR entry index and check for open file descriptors */
+    int idx;
+    for (idx = 0; idx < MAX_FILES; ++idx) {
+        if (strcmp(DIR[idx].name, name) == 0) {
+            if (DIR[idx].ref_count > 0) {
+                debug_dir(idx);
+                perror("fs_delete: open handles for file");
+                return -1;
+            }
+            break;
+        } else if (idx == MAX_FILES - 1) {
+            perror("fs_delete: filename does not exist");
+            return -1;
+        }
+    }
 
-// int fs_read(int fildes, void *buf, size_t nbyte);
-// int fs_write(int fildes, void *buf, size_t nbyte);
+    /* free block allocation in FAT */
+    int head = DIR[idx].head, ante;
+    while (FAT[head] != EOF) {
+        ante = head;
+        head = FAT[head];
+        FAT[ante] = FREE;
+    }
 
-// int fs_get_filesize(int fildes);
-// int fs_listfiles(char ***files);
+    /* clear DIR entry */
+    DIR[idx].used = false;
+    memset(DIR[idx].name, '\0', MAX_FILENAME);
+    DIR[idx].size = 0;
+    DIR[idx].head = -1;
+    DIR[idx].ref_count = 0;
+
+    debug_dir(idx);
+
+    return 0;
+}
+
+int fs_read(int fildes, void *buf, size_t nbyte) {
+    if (fildes < 0 || fildes > MAX_FILDES || fd[fildes].used == false) {  // file descriptor invalid
+        perror("fs_read: file descriptor invalid");
+        return -1;
+    }
+
+    int idx = directory_index(fildes);  // index of directory referred to by fildes
+    int head = DIR[idx].head;
+
+    char *buffer = calloc(1, BLOCK_SIZE); /* allocate buffer */
+
+    if (fd[fildes].offset % BLOCK_SIZE == 0) {  // TODO: this is different -
+    }
+
+    /* traverse till starting block number given offset and copy to buffer */
+    int i;
+    for (i = 0; i < fd[fildes].offset / BLOCK_SIZE; ++i) {  // iter over blocks till block containing end of offset
+        head = FAT[head];                                   // TODO: check that this works
+    }
+
+    /* read data starting from offset */
+    int b = 0;  // bytes actually read
+    for (i = 0; i < (nbyte / BLOCK_SIZE + 1); ++i) {
+        block_read(head, buffer);
+
+        /* CASES 1.first - 3.middle - 2.last */
+        if (i == 0) {
+            b += BLOCK_SIZE - fd[fildes].offset % BLOCK_SIZE;
+            memcpy((void *)buf, (void *)buffer, BLOCK_SIZE - fd[fildes].offset % BLOCK_SIZE);  // last part of first block
+        } else if (i == nbyte / BLOCK_SIZE - 1) {
+            b += (nbyte + fd[fildes].offset) % BLOCK_SIZE;
+            memcpy((void *)buf, (void *)buffer, (nbyte + fd[fildes].offset) % BLOCK_SIZE);  // first part of last block
+        } else {
+            b += BLOCK_SIZE;
+            memcpy((void *)buf, (void *)buffer, BLOCK_SIZE);
+        }
+
+        head = FAT[head];  // iter to next block
+
+        if (head == EOF) {
+            break;
+        }
+    }
+
+    fd[fildes].offset += b; /* implicitly increment 'file pointer' */
+    return b;               // return number of bytes actually read
+}
+
+int fs_write(int fildes, void *buf, size_t nbyte) {
+    // if (fildes < 0 || fildes > MAX_FILDES || fd[fildes].used == false) { /* file descriptor invalid */
+    //     perror("fs_read: file descriptor invalid");
+    //     return -1;
+    // }
+
+    // int idx = directory_index(fildes);
+    // int head = DIR[idx].head;
+
+    // char *buffer = calloc(1, BLOCK_SIZE);
+
+    return 0;  // TODO: number of bytes actually written
+}
+
+// int fs_get_filesize(int fildes) {
+// }
+
+// int fs_listfiles(char ***files) {}
+
 // int fs_lseek(int fildes, off_t offset);
 // int fs_truncate(int fildes, off_t length);
--- fs.h
+++ fs.h
@@ -16,21 +16,21 @@
 
 /* defined in disk.h - DISK_BLOCKS 8192 - BLOCK_SIZE 4096 */
 
-typedef struct superblock_t {  // hardcoded to block 0
+typedef struct superblock {  // hardcoded to block 0
     int dir_idx;               // head of directory
     int dir_len;               // number of blocks of directory
     int fat_idx;               // head of FAT
     int fat_len;               // number of blocks of FAT
     int data_idx;              // first block of file data
-} superblock_t;
+} superblock;
 
-typedef struct dir_entry_t {
+typedef struct dir_entry {
     bool used;                    // is this file-"slot" in use
     char name[MAX_FILENAME + 1];  // mmmm.. donuts
     int size;                     // file size (bytes)
     int head;                     // first data block of file
-    int ref_count;                  // how many open fds (ref_cnt > 0) -> cannot delete file
-} dir_entry_t;
+    int ref_count;                // how many open fds (ref_cnt > 0) -> cannot delete file
+} dir_entry;
 
 typedef struct fd_t {  // file descriptor
     bool used;         // fd in use
@@ -47,8 +47,8 @@ int fs_close(int fildes);
 int fs_create(char *name);
 int fs_delete(char *name);
 
-// int fs_read(int fildes, void *buf, size_t nbyte);
-// int fs_write(int fildes, void *buf, size_t nbyte);
+int fs_read(int fildes, void *buf, size_t nbyte);
+int fs_write(int fildes, void *buf, size_t nbyte);
 
 // int fs_get_filesize(int fildes);
 // int fs_listfiles(char ***files);
